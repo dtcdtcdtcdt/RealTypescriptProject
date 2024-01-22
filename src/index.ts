d0a50b4a32c1054813120834c5f2d236c61b1e10
Daniel Torres Chico SQA <danieltorreschicosqa@Daniels-MacBook-Pro.local>
2024-01-21 23:22:10 -0800

    Private Public

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
+// class User {
+//     public email: string
+//     // private can only be used within the class
+//     private name: string
+//     private readonly city: string = "Long Beach"
+//     constructor(email: string, name: string) {
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
+
+// How it's generally done, without this keywords
 class User {
-    email: string
-    name: string
-    readonly city: string = "Long Beach"
-    constructor(email: string, name: string) {
-        this.email = email;
-        this.name = name;
+    constructor(public email: string, public name: string) {
     }
 }
 
 const daniel = new User("daniel@gmail.com", "Daniel")
 // daniel.city = "Long Beach"
+// daniel.name 
